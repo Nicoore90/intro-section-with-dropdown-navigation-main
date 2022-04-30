d15be2ce240ff79b14d6c38b72cb9957b204029f
Nicolas Orecchia <nicoaleore@gmail.com>
2022-04-30 19:21:27 -0300

    navbar desktop y dropdown mobile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import { Header } from "./components/header/header"
+import { Header, Dropdown } from "./components/header/header"
 import heromobile from "./image-hero-mobile.png"
 import herodesktop from "./image-hero-desktop.png"
 import { Text } from "./components/text/text"
 import { Footer } from './components/footer/footer';
 
-function App() {
+export function App() {
   return (
     <div className="App">
-      <Header />
+      <Header><Dropdown /></Header>
       <div className='content'>
         <img src={heromobile} className="hero-mobile"/>
         <img src={herodesktop} className="hero-desktop" />
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,48 @@
 import "./header.css"
 import logo from "./assets/logo.svg"
 import menu from "./assets/icon-menu.svg"
+import React, { useState } from 'react'
+import { Fragment } from "react"
 
-export function Header() {
+export function Header(props) {
+
+    const [open, setOpen] = useState(false)
+
+
     return(
         <header>
             <nav>
-                <ul>
-                    <li className="logo"><img src={logo} /></li>
-                    <li className="menu">Features</li>
-                    <li className="menu">Company</li>
-                    <li className="menu">Careers</li>
-                    <li className="menu">About</li>
-                    <li className="menu-closed"><img src={menu} /></li>
-                    <li className="login">Login</li>
-                    <li className="button"><button>Register</button></li>
+                <ul> 
+                    <div className="menu__main">
+                        <li className="logo"><img src={logo} /></li>
+                        <li className="menu">Features</li>
+                        <li className="menu">Company</li>
+                        <li className="menu">Careers</li>
+                        <li className="menu">About</li>
+                    </div>
+                    <li className="menu-closed"><img src={menu} onClick={() => setOpen(!open)}/>
+                        { open && props.children }</li>
+                    <div className="menu__enter">
+                        <li className="login">Login</li>
+                        <li className="button"><button>Register</button></li>
+                    </div>
                 </ul>
             </nav>
         </header>
+    )
+}
+
+export function Dropdown() {
+
+
+    return (
+        <div className="dropdown__container">
+            <li className="dropdown">Features</li>
+            <li className="dropdown">Company</li>
+            <li className="dropdown">Careers</li>
+            <li className="dropdown">About</li>
+            <li className="dropdown__login">Login</li>
+            <li className="dropdown__register"><button>Register</button></li>
+        </div>
     )
 }
